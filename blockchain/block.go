264f183973380cafe2bac50d79a6b52f2c63981b
satoshisong01 <rudtjrch@naver.com>
2021-07-08 10:34:03 +0900

    first mining

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,16 +4,21 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/sks8982/snackcoin/db"
 	"github.com/sks8982/snackcoin/utils"
 )
 
+const difficulty int = 2 //hash 앞에 0의 갯수
+
 type Block struct {
-	Data     string `json:"data"`
-	Hash     string `json:"hash"`
-	PrevHash string `json:"prevHas,omitempty"`
-	Height   int    `json:"height"`
+	Data       string `json:"data"`
+	Hash       string `json:"hash"`
+	PrevHash   string `json:"prevHas,omitempty"`
+	Height     int    `json:"height"`
+	Difficulty int    `json:"difficulty"`
+	Nonce      int    `json:"nonce"`
 }
 
 func (b *Block) persist() { // block을 저장하기 위해만든 SaveBlock함수 호출
@@ -36,15 +41,32 @@ func FindBlock(hash string) (*Block, error) { //hash를 받고  Block포인터 �
 	return block, nil
 }
 
+//채굴
+func (b *Block) mine() {
+	target := strings.Repeat("0", b.Difficulty)
+	for {
+		blockAsString := fmt.Sprint(b)                                  //블록 string생성
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString))) //string을 hash로바꾸고 hash를 다시 16진수로 바꿈
+		fmt.Printf("Block as String:%s\nHash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
+		if strings.HasPrefix(hash, target) { //target이랑 동일한 형태로 시작하는지 확인
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string, prevHash string, height int) *Block {
 	block := Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.mine()
 	block.persist()
 	return &block
 }
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/sks8982/snackcoin/cli"
+	"github.com/sks8982/snackcoin/db"
 )
 
 func main() {
+	defer db.Close()
 	cli.Start()
 }
